b907e3f8f3d1c31532b3997dcf6d3f0a689f6d67
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-16 02:43:33 -0500

    Let the user update his profile.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :height])
+    user_keys = [:first_name, :last_name, :height]
+    devise_parameter_sanitizer.permit(:sign_up, keys: user_keys)
+    devise_parameter_sanitizer.permit(:sign_in, keys: user_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: user_keys)
   end
 
   def after_sign_in_path_for(resource)
