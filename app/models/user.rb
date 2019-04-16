9fef82534e47f8588caf08b80588508a139dcef6
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-15 23:35:13 -0500

    Add association for bmi_calculation model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :bmi_calculations
+
   validates_presence_of :first_name, :last_name, :height
   validates_numericality_of :height, size: { greater_than: 0 }
 end
