04274e266cb20a752b3f519f45b0a693f2212ba3
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-16 01:20:24 -0500

    Show user history inside the dashboard action.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,8 @@ class WelcomeController < ApplicationController
 
   def index; end
 
-  def dasboard; end
+  def dashboard
+    @bmi_calculations = current_user.bmi_calculations.order(:id)
+    @last_bmi = @bmi_calculations.last.try(:index)
+  end
 end
